3cfda4dbd9a9673bcdfc384cb298fb4167571601
crkershaw <crkershaw@gmail.com>
2022-02-04 13:39:45 -0500

    Hangman calls to API for results

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,6 +19,8 @@ func main() {
 
 	router.GET("/", homepage)
 	router.GET("/hangman", hangman)
+	router.GET("/hangman/api/wrdlen/:id", hangman_api_lengthcheck)
+	router.GET("/hangman/api/ltrchk/:id", hangman_api_lettercheck)
 
 	router.Run(":80")
 
@@ -29,4 +33,56 @@ func homepage(c *gin.Context) {
 
 func hangman(c *gin.Context) {
 	c.HTML(http.StatusOK, "hangman.html", []string{})
+}
+
+func hangman_api_lengthcheck(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range wordlists {
+		if a.Id == id {
+			// fmt.Println(a)
+			var word = a.Words[word_index]
+			c.IndentedJSON(http.StatusOK, len(word))
+			return
+		}
+	}
+}
+
+func hangman_api_lettercheck(c *gin.Context) {
+	id := c.Param("id")
+	letter := strings.ToUpper(c.Query("letter"))
+
+	fmt.Println("Received letter check api call, ID: " + string(id) + ", Letter: " + string(letter))
+
+	for _, a := range wordlists {
+
+		var answer_list = map[int]string{}
+
+		if a.Id == id {
+			var word = strings.ToUpper(a.Words[word_index])
+
+			for index, value := range word {
+
+				if string(value) == string(letter) {
+					answer_list[index] = string(value)
+				}
+			}
+
+		}
+
+		c.IndentedJSON(http.StatusOK, answer_list)
+		return
+	}
+}
+
+type wordlist struct {
+	Id    string   `json:"id"`
+	Words []string `json:"words"`
+}
+
+var word_index = 2
+var words = []string{"doggo", "foxy", "wiggly", "blasty", "tufferina", "england", "apartment"}
+
+var wordlists = []wordlist{
+	{Id: "abc123", Words: words},
 }
--- public/js/hangman.js
+++ public/js/hangman.js
@@ -9,37 +9,56 @@ class Hangman extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            answer_length: api_lengthcheck(),
+            // answer_length: api_lengthcheck().then(response => {return response}),
+            answer_length: null,
             answer_letters: {},
             guessed_letters: [],
             complete: false
         }
     }
 
-    check_letter = (letter) => {
-        this.setState(prevState => ({guessed_letters: [...prevState.guessed_letters, letter]}))
-        var result = api_lettercheck(letter)
+    componentDidMount(){
+        this.api_lengthcheck();
+    }
 
-        // Create an object of the new answer letters revealed
-        var answer_letters_toadd = {}
-        for(const [key, value] of Object.entries(result)) {
-            answer_letters_toadd = {...answer_letters_toadd, [key]: value}
-        }
+    api_lengthcheck = () => {
+        return fetch("/hangman/api/wrdlen/abc123")
+            .then(response => response.json())
+            .then(data => this.setState({answer_length: data}))
+            .catch(error => console.log(error));
+    }
+    
+    api_lettercheck = (letter) => {  // Anonymous function so we can call this.State
+        return fetch("/hangman/api/ltrchk/abc123?letter=" + letter)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
 
-        // Combine that object with the existing state object (note: can't change state in-place)
-        const answer_letters_new = {...this.state.answer_letters, ...answer_letters_toadd}
-        this.setState(
-            {answer_letters: answer_letters_new},
-            function() { // Callback function so it ensures state is updated before running
-                if(Object.keys(this.state.answer_letters).length == this.state.answer_length){
-                    this.setState({complete: true})
+                // Adding letter to list of guessed letters
+                this.setState(prevState => ({guessed_letters: [...prevState.guessed_letters, letter]}))
+
+                // Create an object of the new answer letters revealed
+                var answer_letters_toadd = {}
+                for(const [key, value] of Object.entries(data)) {
+                    answer_letters_toadd = {...answer_letters_toadd, [key]: value}
                 }
-            })
 
+                // Combine that object with the existing state object (note: can't change state in-place)
+                const answer_letters_new = {...this.state.answer_letters, ...answer_letters_toadd}
+                this.setState(
+                    {answer_letters: answer_letters_new},
+                    function() { // Callback function so it ensures state is updated before running
+                        if(Object.keys(this.state.answer_letters).length == this.state.answer_length){
+                            this.setState({complete: true})
+                        }
+                    })
+
+            })
     }
 
     reset_hangman = () => {
-        this.setState({answer_length: api_lengthcheck(), answer_letters: {}, guessed_letters: [], complete: false})
+        this.api_lengthcheck()
+        this.setState({answer_letters: {}, guessed_letters: [], complete: false})
     }
 
     render(){
@@ -49,7 +68,7 @@ class Hangman extends React.Component{
             {className: "hangman"},
             [
                 e(HgmnWord, {answer_length: this.state.answer_length, answer_letters: this.state.answer_letters }),
-                e(KeyBoard, {guessed_letters: this.state.guessed_letters, onHandleClick: this.check_letter }), // Sending function down to keyboard
+                e(KeyBoard, {guessed_letters: this.state.guessed_letters, onHandleClick: this.api_lettercheck }), // Sending function down to keyboard
                 e(Finish, {complete: this.state.complete, onHandleClick: this.reset_hangman})
             ]
         )
@@ -63,21 +82,3 @@ ReactDOM.render(e(Hangman, {answer_word: answer_word}), hangman);
 
 
 
-function api_lengthcheck(){
-    return answer_word.length
-}
-function api_lettercheck(letter){
-
-    var answer_word_upper = answer_word.toUpperCase();
-
-    var answer_list = {}
-
-    for(let i=0; i<answer_word_upper.length; i++){
-        if(answer_word_upper[i] == letter){
-            answer_list[i] = letter
-        }
-    }
-
-    return answer_list
-
-}
